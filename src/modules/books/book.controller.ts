2d5db0b5e8ba77a53b33d77e3ce2482d8a16ed1e
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2025-07-09 12:24:17 -0300

    fix: add ApiBearerAuth

--- src/modules/books/book.controller.ts
+++ src/modules/books/book.controller.ts
@@ -1,18 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CreateBookDto } from './dto/create-book.dto';
 import { BookService } from './book.service';
 import { SuccessResponse } from '@common/common.interfaces';
 import { SwaggerCreateBookDocs } from './docs/book.docs';
 
 @ApiTags('Book')
+@ApiBearerAuth()
 @Controller('books')
 export class BookController {
   constructor(private bookService: BookService) {}
 
   @SwaggerCreateBookDocs()
   @Post()
-  register(@Body() dto: CreateBookDto): Promise<SuccessResponse> {
+  create(@Body() dto: CreateBookDto): Promise<SuccessResponse> {
     return this.bookService.createBook(dto);
   }
 
